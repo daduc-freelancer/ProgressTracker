07391b53477640d4f5c134d8306ad63e42837492
DAD <daduc.2308@gmail.com>
2025-06-04 15:28:19 +0700

    create .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyBwYIF16HrP9vsED5oXWwlbZd1uk-QB-H0",
-	authDomain: "progresstracker-f3988.firebaseapp.com",
-	projectId: "progresstracker-f3988",
-	storageBucket: "progresstracker-f3988.firebasestorage.app",
-	messagingSenderId: "122551495157",
-	appId: "1:122551495157:web:4fe4be78fc9f00418a58bc",
-	measurementId: "G-VDM0MDSEQ9",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
